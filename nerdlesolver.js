652d76083489b1d528a129c8668bb383cb36d37d
RemmyLee <detroitguy@gmail.com>
2023-02-04 17:23:28 -0500

    Update nerdlesolver.js

--- nerdlesolver.js
+++ nerdlesolver.js
@@ -125,66 +125,33 @@ function parseExpression(kExpression, nOffset = 0, eSolutionType = E_SOLUTION_NA
   return [true, nResult];
 }
 
-
-
-
-
-
-function processExpression()
-{
-    var bValid                  = true;
+function processExpression() {
+    var bValid = true,
+        kExpression = "";
     kIssuesTextNode.textContent = "No Issues";
-    kExpression                    = "";
-
-    // By default, everything is valid
-    for (var i = 0; i < kCurrentRow.length; ++i)
-    {
+    for (var i = 0; i < kCurrentRow.length; ++i) {
         kCurrentRow[i].classList.remove(E_NODE_INVALID);
         kCurrentRow[i].classList.add(E_NODE_POSITION_UNKNOWN);
+        kExpression += kCurrentRow[i].childNodes[0].textContent;
     }
-
-    //////////////////////////////////////////////////////////////////
-    // Simple Test 1
-    //
-    // Any characters that aren't supposed to be there
-    for (var i = 0; i < kCurrentRow.length; ++i)
-    {
-        const kChar = kCurrentRow[i].childNodes[0].textContent;
-        if (!kValidChars.includes(kChar))
-        {
+    for (var i = 0; i < kCurrentRow.length; ++i) {
+        if (!kValidChars.includes(kCurrentRow[i].childNodes[0].textContent)) {
             kCurrentRow[i].classList.add(E_NODE_INVALID);
             kCurrentRow[i].classList.remove(E_NODE_POSITION_UNKNOWN);
             bValid = false;
         }
-        kExpression += kCurrentRow[i].childNodes[0].textContent;
     }
-
-    if (!bValid)
-    {
+    if (!bValid) {
         kIssuesTextNode.textContent = "Incomplete entry (shown in red)";
-        return false
+        return false;
     }
-
-    //////////////////////////////////////////////////////////////////
-    // Simple Test 2
-    //
-    // Missing Equals
-    if (!kExpression.includes("="))
-    {
+    if (!kExpression.includes("=")) {
         kIssuesTextNode.textContent = "No Equality '=' entry";
         return false;
     }
-
-    //////////////////////////////////////////////////////////////////
-    // Simple Test 3
-    //
-    // Multiple Equals
-    if (kExpression.split("=").length != 2)
-    {
-        for (var i = 0; i < kExpression.length; ++i)
-        {
-            if (kExpression.charAt(i) == "=")
-            {
+    if (kExpression.split("=").length != 2) {
+        for (var i = 0; i < kExpression.length; ++i) {
+            if (kExpression.charAt(i) == "=") {
                 kCurrentRow[i].classList.add(E_NODE_INVALID);
                 kCurrentRow[i].classList.remove(E_NODE_POSITION_UNKNOWN);
             }
@@ -192,113 +159,29 @@ function processExpression()
         kIssuesTextNode.textContent = "Too many '=' entries";
         return false;
     }
-    
-    if (false == bInstant)
-    {
-        //////////////////////////////////////////////////////////////////
-        // Simple Test 4
-        //
-        // Equals at Beginning or End of Expression
-        const nEqualityIndex = kExpression.indexOf("=")
-        if ((nEqualityIndex == 0) || (nEqualityIndex == (kCurrentRow.length-1)))
-        {
+    if (false == bInstant) {
+        const nEqualityIndex = kExpression.indexOf("=");
+        if (nEqualityIndex == 0 || nEqualityIndex == kCurrentRow.length - 1) {
             kIssuesTextNode.textContent = "Invalid position for equality '='";
             kCurrentRow[nEqualityIndex].classList.add(E_NODE_INVALID);
             kCurrentRow[nEqualityIndex].classList.remove(E_NODE_POSITION_UNKNOWN);
             return false;
         }
-        
-        //////////////////////////////////////////////////////////////////
-        // Simple Test 4
-        //
-        // First character isn't a numeric, unary plus or minus
-        if (!kValidNumbersWithSign.includes(kExpression.charAt(0)))
-        {
+        if (!kValidNumbersWithSign.includes(kExpression.charAt(0))) {
             kIssuesTextNode.textContent = "First character must be one of the following: +, - or a number";
             kCurrentRow[0].classList.add(E_NODE_INVALID);
             kCurrentRow[0].classList.remove(E_NODE_POSITION_UNKNOWN);
             return false;
         }
-        
-        //////////////////////////////////////////////////////////////////
-        // Simple Test 5
-        //
-        // First after the equality statement isn't a numeric, unary plus or minus
-        if (!kValidNumbersWithSign.includes(kExpression.charAt(nEqualityIndex+1)))
-        {
+        if (!kValidNumbersWithSign.includes(kExpression.charAt(nEqualityIndex + 1))) {
             kIssuesTextNode.textContent = "First character after the equals '=' sign must be one of the following: +, - or a number";
-            kCurrentRow[nEqualityIndex+1].classList.add(E_NODE_INVALID);
-            kCurrentRow[nEqualityIndex+1].classList.remove(E_NODE_POSITION_UNKNOWN);
-            return false;
-        }
-        
-        //////////////////////////////////////////////////////////////////
-        // Simple Test 6
-        //
-        // Right Side of the Equality cannot contain an expression
-        //
-        // Note: This isn't actually true, Nerdles parser actually allows unary stacking
-        //       i.e. -+5 which evalues to -5 is allowed.
-        //            This is silly, but it's how Nerdle works for now.......
-        bValid              = true;
-        var bAnyNumberFound = false;
-        var kValidityString = kValidNumbersWithSign;
-        
-        for (var i = nEqualityIndex+1; i < kExpression.length; ++i)
-        {
-            const kChar = kExpression.charAt(i);
-            if (false == bAnyNumberFound)
-            {
-                if (kValidNumbers.includes(kChar))
-                {
-                    bAnyNumberFound = true;
-                    kValidityString = kValidNumbers;
-                }
-            }
-
-            if (!kValidityString.includes(kChar))
-            {
-                kCurrentRow[i].classList.add(E_NODE_INVALID);
-                kCurrentRow[i].classList.remove(E_NODE_POSITION_UNKNOWN);
-                bValid = false;
-            }
-        }
-
-        if (!bValid)
-        {
-            kIssuesTextNode.textContent = "Right side of the equals '=' cannot contain an expression (operators shown in red)";
-            return false;
-        }
-
-        //////////////////////////////////////////////////////////////////
-        // Non Trivial Test
-        //
-        // Evaluate the Left/Right Side of the equals statement, and check
-        // for validity / equality
-        const kResultLeft = parseExpression(kExpression.substring(0, nEqualityIndex), 0);
-        if (!kResultLeft[0])
-        {
-            return false;
-        }
-
-        const kResultRight = parseExpression(kExpression.substring(nEqualityIndex+1), nEqualityIndex+1);
-        if (!kResultRight[0])
-        {
-            return false;
-        }
-
-        if (kResultLeft[1] != kResultRight[1])
-        {
-            kCurrentRow[nEqualityIndex].classList.add(E_NODE_INVALID);
-            kCurrentRow[nEqualityIndex].classList.remove(E_NODE_POSITION_UNKNOWN);
-            kIssuesTextNode.textContent = "Expression is not equals.  Left side = " + kResultLeft[1] + ", Right Side = " + kResultRight[1] + ".";
+            kCurrentRow[nEqualityIndex + 1].classList.add(E_NODE_INVALID);
+            kCurrentRow[nEqualityIndex + 1].classList.remove(E_NODE_POSITION_UNKNOWN);
             return false;
         }
     }
-    
     return true;
 }
-
 
 
 
